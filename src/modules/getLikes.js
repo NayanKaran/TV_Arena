3f07ea412bc3c59272dee68a3c2d290ba0526831
Olawale <olapetanwale@outlook.com>
2022-06-22 19:27:43 +0100

    Fix linter error

--- src/modules/getLikes.js
+++ src/modules/getLikes.js
@@ -1,17 +1,17 @@
 const getLikes = async () => {
-  const apiUrl = `https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/a2tSbPCwHIrhAyfjwKEq/likes`;  
+  const apiUrl = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/a2tSbPCwHIrhAyfjwKEq/likes';
   const response = await fetch(apiUrl);
   const data = response.json();
   return data;
 };
 
 const displayLikes = async () => {
-  const likes = await getLikes();  
+  const likes = await getLikes();
   likes.forEach((item) => {
-    const likespara = document.getElementById(`${item.item_id}`);   
-    if (item.item_id){        
-        likespara.textContent = `${item.likes}`;
-    }    
+    const likespara = document.getElementById(`${item.item_id}`);
+    if (item.item_id) {
+      likespara.textContent = `${item.likes}`;
+    }
   });
 };
 
